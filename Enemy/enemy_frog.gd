9e0cae967a59b349fda4ac75654f0efac1f009f4
GitVanHove <92695604+GitVanHove@users.noreply.github.com>
2024-11-14 19:09:16 +0100

    Frog animation
    Changed frog idle animation to be a little longer and more natural.

--- Enemy/enemy_frog.gd
+++ Enemy/enemy_frog.gd
@@ -12,13 +12,12 @@ func _physics_process(delta: float) -> void:
 	#Gravity frog
 	velocity += get_gravity() * delta
 	if chase == true:
+		anim.play("Jump")
 		var direction = (player.position - self.position).normalized()
 		if direction.x > 0:
 			get_node("AnimatedSprite2D").flip_h = true
-			anim.play("Jump")
 		else:
 			get_node("AnimatedSprite2D").flip_h = false
-			anim.play("Jump")
 		velocity.x = direction.x * SPEED	
 	else:
 		anim.play("Idle")
