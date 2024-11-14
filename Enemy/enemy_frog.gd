0ea939f9543010e32d719213ac017497563c646d
GitVanHove <92695604+GitVanHove@users.noreply.github.com>
2024-11-14 19:06:30 +0100

    Frog mob AI
    Wrote very basic frog mob ai to chase and follow player when it enters it's detection range and stop when the player leaves.

--- Enemy/enemy_frog.gd
+++ Enemy/enemy_frog.gd
@@ -4,21 +4,26 @@ extends CharacterBody2D
 
 
 @onready var player = get_node("../../Player/Player")
+@onready var anim = get_node("AnimatedSprite2D")
 var chase = false
 var SPEED = 50
 
 func _physics_process(delta: float) -> void:
 	#Gravity frog
 	velocity += get_gravity() * delta
-	var direction = (player.position - self.position).normalized()
-	if direction.x > 0:
-		if chase == true:
-			velocity.x = direction.x * SPEED
-			print("right")
+	if chase == true:
+		var direction = (player.position - self.position).normalized()
+		if direction.x > 0:
+			get_node("AnimatedSprite2D").flip_h = true
+			anim.play("Jump")
+		else:
+			get_node("AnimatedSprite2D").flip_h = false
+			anim.play("Jump")
+		velocity.x = direction.x * SPEED	
 	else:
-		if chase == true:
-			print("left")
-	
+		anim.play("Idle")
+		velocity.x = 0
+		
 	move_and_slide()	
 
 
